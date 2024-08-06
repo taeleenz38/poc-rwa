a0f7c9bfeb76d50fbbed2fca80c24341170a5568
dilum101 <70141528+dilum101@users.noreply.github.com>
2024-08-06 10:44:15 +1000

    redemption

--- frontend/src/app/assetsender/page.tsx
+++ frontend/src/app/assetsender/page.tsx
@@ -1,9 +1,52 @@
-import React from 'react'
+"use client";
+
+import React from "react";
+import { EthIcon } from "../components/atoms/Icons";
+import Contact from "../components/molecules/Contact";
+import FundDetails from "../components/organisms/FundDetails";
+import SetPrice from "../components/organisms/Popups/SetPrice";
+import UpdatePrice from "../components/organisms/Popups/UpdatePrice";
 
 const page = () => {
-  return (
-    <div>page</div>
-  )
-}
+  const [isSetPriceOpen, setIsSetPriceOpen] = React.useState(false);
+  const [isUpdatePriceOpen, setIsUpdatePriceOpen] = React.useState(false);
+  const handleButton1Click = () => {};
 
-export default page
+  return (
+    <div className="min-h-screen bg-white">
+      <FundDetails
+        logoSrc="/LOGO.png"
+        altText="Fund logo"
+        fundName="AYF"
+        fundDescription="Copiam Australian Yield Fund"
+        yieldText="Stable, high-quality Australian Yield Fund"
+        price="$1.0445"
+        tvl="$327.50M"
+        Button1Text="Approve Redeem"
+        Button2Text="Hidden "
+        Button1Class="bg-light text-primary hover:bg-primary hover:text-light"
+        Button2Class="bg-secondary text-light hover:bg-primary hidden"
+        onButton1Click={handleButton1Click}
+        onButton2Click={() => {}}
+        chains={
+          <>
+            <EthIcon className="lg:w-8 lg:h-8" />
+          </>
+        }
+        view={"RedemptionList"}
+      />
+
+      <Contact />
+      <SetPrice
+        isOpen={isSetPriceOpen}
+        onClose={() => setIsSetPriceOpen(false)}
+      />
+      <UpdatePrice
+        isOpen={isUpdatePriceOpen}
+        onClose={() => setIsUpdatePriceOpen(false)}
+      />
+    </div>
+  );
+};
+
+export default page;
---
+++ frontend/src/app/components/molecules/RedemptionList.tsx
@@ -0,0 +1,75 @@
+import React, { useEffect, useState } from "react";
+import Button from "@/app/components/atoms/Buttons/Button";
+
+type RedemptionList = {
+  user: string;
+  redemptionId: string;
+  redeemAmount: number;
+  rwaAmountIn: string;
+};
+
+const RedemptionList: React.FC = () => {
+  const [redemtions, setRedemptions] = useState<RedemptionList[]>([]);
+  const [Loaded, setLoaded] = useState(false);
+
+  useEffect(() => {
+    const fetchPriceList = async () => {
+      try {
+        setLoaded(false);
+        const response = await fetch(
+          `${process.env.NEXT_PUBLIC_BACKEND_API}/pending-approval-redemption-list`
+        );
+
+        console.log(response, "response");
+        const data = await response.json();
+        setRedemptions(data);
+      } catch (error) {
+        console.error("Error fetching price list:", error);
+      } finally {
+        setLoaded(true);
+      }
+    };
+
+    fetchPriceList();
+  }, []);
+
+  return (
+    <div className="w-full h-3/4 p-4 mx-auto text-primary bg-white flex flex-col mt-8 rounded-md bg-hero-pattern bg-no-repeat bg-right-bottom">
+      <div className="text-3xl font-semibold mb-6 text-center mt-4">
+        Redemption List
+      </div>
+      <div className="flex flex-col gap-y-3 h-[80vh] overflow-y-scroll rounded-md p-4">
+        {!Loaded && (
+          <p className="flex justify-center items-center ">
+            <strong>Loading items...</strong>
+          </p>
+        )}
+
+        {redemtions.length === 0 && Loaded && (
+          <p className="flex justify-center items-center ">
+            <strong>No Redemption Items found</strong>
+          </p>
+        )}
+
+        {redemtions.map((item) => (
+          <div key={item.user} className="p-4 rounded-lg bg-primary text-light">
+            <div>
+              <h3 className="text-lg font-medium mb-2">User: {item.user}</h3>
+              <p>
+                <strong>Redemption Id:</strong> {item.redemptionId}
+              </p>
+              <p>
+                <strong>Redeem Amount:</strong> {item.redeemAmount}
+              </p>
+              <p>
+                <strong>RWA Amount In:</strong> {item.rwaAmountIn}
+              </p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default RedemptionList;
