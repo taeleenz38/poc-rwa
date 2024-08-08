00720cb402629f7b7c2bc71bdac6f2495aadcc83
dilum101 <70141528+dilum101@users.noreply.github.com>
2024-08-08 13:07:20 +1000

    priceList

--- frontend/src/app/allowlist/page.tsx
+++ frontend/src/app/allowlist/page.tsx
@@ -14,125 +14,19 @@ import {
 import Button from "../components/atoms/Buttons/Button";
 import axios from "axios";
 import AllowlistPopUp from "@/app/components/organisms/Popups/AllowlistPopUp";
-
-interface AccountStatusResponse {
-  termIndex: string;
-  account: string;
-  status: boolean;
-}
+import AllowlistTab from "../components/organisms/AllowListTab";
 
 const Allowlist = () => {
-  const [wallets, setWallets] = useState<AccountStatusResponse[]>([]);
-  const [isTableLoading, setIsTableLoading] = useState(false);
-  const [addAddreesOpen, setAddAddressOpen] = useState(false);
-
-  useEffect(() => {
-    const fetchWallets = async () => {
-      try {
-        setIsTableLoading(true);
-        const response = await axios.get(
-          `${process.env.NEXT_PUBLIC_BACKEND_API}/account-status`
-        );
-        setWallets(response.data);
-      } catch (error) {
-        console.error("Error fetching wallets:", error);
-      } finally {
-        setIsTableLoading(false);
-      }
-    };
-
-    fetchWallets();
-  }, []);
-
   return (
-    <div className="flex flex-col px-28  items-start w-full h-full pt-6 pb-6 ">
-      <div className="flex flex-col gap-4">
-        <h1 className="text-5xl font-semibold text-black ">Admin Requests</h1>
-        <h2 className="text-2xl font-semibold text-gray mb-10">
-          Track and manage your requests
-        </h2>
-      </div>
-
-      <>
-        <div
-          className={`p-6 border-b-2  border-[#F5F2F2]bg-[#F5F2F2] border-b-[#ba745f] text-[#ba745f] bg-secondary/20`}
-        >
-          Manage AllowList
-        </div>
-        <div className="flex flex-col w-full border p-4">
-          <div className="flex w-full justify-around items-center py-6">
-            <Button
-              text={"Add New Address"}
-              onClick={() => setAddAddressOpen(true)}
-              className="bg-primary py-2 text-light hover:bg-light hover:text-primary rounded-md"
-            />
-            <Button
-              text={"Update Existing Price ID"}
-              onClick={() => {}}
-              className="bg-primary py-2 text-light hover:bg-light hover:text-primary rounded-md"
-              disabled
-            />
-          </div>
-
-          <div className="overflow-x-auto pt-4">
-            <table className="table">
-              <thead>
-                <tr className="text-gray text-lg bg-[#F5F2F2] border-none ">
-                  <th className="text-center">ID#</th>
-                  <th className="text-center">User</th>
-                  <th className="text-center">Status</th>
-                  {/* <th className="text-center">Transaction Date</th> */}
-                  <th className="text-center">Update</th>
-                </tr>
-              </thead>
-              <tbody>
-                {isTableLoading ? (
-                  <tr>
-                    <td
-                      colSpan={4}
-                      className="py-10 font-medium text-gray text-center"
-                    >
-                      Loading data...
-                    </td>
-                  </tr>
-                ) : !isTableLoading && wallets.length === 0 ? (
-                  <tr>
-                    <td
-                      colSpan={4}
-                      className="py-10 font-medium text-gray text-center"
-                    >
-                      No wallet added to allowlist
-                    </td>
-                  </tr>
-                ) : (
-                  wallets.map((wallet: AccountStatusResponse) => (
-                    <tr className="border-b-2 border-[#F5F2F2] font-medium text-gray">
-                      <td className="text-center">{wallet.termIndex}</td>
-                      <td className="text-center">{wallet.account}</td>
-                      <td className="text-center">
-                        {wallet.status === true ? "Active" : "Paused"}
-                      </td>
-                      {/* <td>Transaction Date</td> */}
-                      <td className="text-center">
-                        <div className="flex justify-center items-center ">
-                          <input
-                            type="checkbox"
-                            className="checkbox checkbox-secondary"
-                          />
-                        </div>
-                      </td>
-                    </tr>
-                  ))
-                )}
-              </tbody>
-            </table>
-          </div>
-          <AllowlistPopUp
-            isOpen={addAddreesOpen}
-            onClose={() => setAddAddressOpen(false)}
-          />
-        </div>
-      </>
+    <div className="min-h-screen root-container">
+      <h1 className="text-5xl font-semibold text-black mb-6">
+        AllowList Management
+      </h1>
+      <h2 className="text-2xl font-semibold text-gray mb-12">
+        Manage list of all allowed users
+      </h2>
+      <AllowlistTab />
+      {/* <PricingSection /> */}
     </div>
   );
 };
--- frontend/src/app/components/organisms/Popups/RemoveAllowListPopUp.tsx
+++ frontend/src/app/components/organisms/Popups/RemoveAllowListPopUp.tsx
@@ -1,10 +1,10 @@
 "use client";
-import { BigNumber } from "ethers";
+import { ethers } from "ethers";
 import { useState, useEffect } from "react";
 import InputField from "@/app/components/atoms/Inputs/TextInput";
 import CloseButton from "@/app/components/atoms/Buttons/CloseButton";
 import Submit from "@/app/components/atoms/Buttons/Submit";
-import abi from "@/artifacts/Pricer.json";
+import abi from "@/artifacts/IAllowlist.json";
 import {
   useWriteContract,
   useSignMessage,
@@ -12,52 +12,44 @@ import {
 } from "wagmi";
 import { config } from "@/config";
 
-interface UpdatePriceProps {
+interface AllowlistProps {
   isOpen: boolean;
   onClose: () => void;
-  priceId: string;
+  walletAddress: string;
 }
 
-const UpdatePrice: React.FC<UpdatePriceProps> = ({
+const RemoveAllowListPopUp: React.FC<AllowlistProps> = ({
   isOpen,
   onClose,
-  priceId,
+  walletAddress,
 }) => {
-  const [updatePrice, setUpdatePrice] = useState<string>("");
-  const [txHash, setTxHash] = useState<string>("");
+  //   const [walletAddress, setWalletAddress] = useState<string | null>(null);
+  const [txHash, setTxHash] = useState<string | null>(null);
   const { writeContractAsync, isPending } = useWriteContract({ config });
 
   const resetForm = () => {
-    setUpdatePrice("");
+    setTxHash(null);
   };
   const onCloseModal = () => {
     onClose();
     resetForm();
   };
 
-  const onPriceIdChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setUpdatePrice(e.target.value);
-  };
-
-  const onPriceChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setUpdatePrice(e.target.value);
-  };
-
-  const handleUpdatePrice = async () => {
-    const priceID = BigNumber.from(priceId).mul(BigNumber.from(10).pow(18));
-
-    const price = BigNumber.from(updatePrice).mul(BigNumber.from(10).pow(18));
+  const handleAllowlistRemove = async () => {
     try {
       const tx = await writeContractAsync({
         abi: abi.abi,
-        address: process.env.NEXT_PUBLIC_PRICER_ADDRESS as `0x${string}`,
-        functionName: "updatePrice",
-        args: [priceID, price],
+        address: process.env.NEXT_PUBLIC_ALLOWLIST_ADDRESS as `0x${string}`,
+        functionName: "setAccountStatus",
+        args: [walletAddress as `0x${string}`, 0, false],
       });
       setTxHash(tx);
-      console.log("Price successfully updated - transaction hash:", tx);
+      console.log(
+        "wallet address successfully removed - transaction hash:",
+        tx
+      );
     } catch (error) {
-      console.error("Error updating price:", error);
+      console.error("Error removing  allowlist:", error);
     }
   };
 
@@ -69,25 +61,22 @@ const UpdatePrice: React.FC<UpdatePriceProps> = ({
 
   return (
     <div className="fixed inset-0 bg-black bg-opacity-30 z-50 flex justify-center items-center">
-      <div className="p-8 rounded-lg text-light bg-primary border-2 border-light shadow-md shadow-white w-1/3">
+      <div className="p-6 rounded-lg text-light bg-primary border-2 border-light shadow-md shadow-white w-1/3">
         <div className="flex justify-between items-center mb-8">
           <div></div>
-          <h2 className="text-3xl font-bold">Update Price</h2>
+          <h2 className="text-3xl text-white font-bold">
+            Remove User From Allowlist
+          </h2>
           <CloseButton onClick={onCloseModal} />
         </div>
         <div className="text-center px-8 text-xl mb-4 font-medium">
-          Please enter Price and Price ID for which you want to update.
+          Please confirm to remove user from allow list
         </div>
         <div className="w-full mx-auto mb-8">
           <InputField
-            label="Price ID:"
-            value={priceId}
-            onChange={onPriceIdChange}
-          />{" "}
-          <InputField
-            label="Price:"
-            value={updatePrice || ""}
-            onChange={onPriceChange}
+            label="Wallet Address:"
+            value={walletAddress}
+            onChange={() => {}}
           />
         </div>
         <div className="w-full flex justify-between">
@@ -101,8 +90,8 @@ const UpdatePrice: React.FC<UpdatePriceProps> = ({
           </div>
           <div className="w-[49%]">
             <Submit
-              onClick={handleUpdatePrice}
-              label={isPending ? "Confirming..." : "Confirm"}
+              onClick={handleAllowlistRemove}
+              label={isPending ? "Confirming..." : "Remove User"}
               disabled={isPending || isLoading}
               className="w-full"
             />
@@ -123,4 +112,4 @@ const UpdatePrice: React.FC<UpdatePriceProps> = ({
   );
 };
 
-export default UpdatePrice;
+export default RemoveAllowListPopUp;
