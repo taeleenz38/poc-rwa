af9e48b3a258806ce812f9473176cca448f29b1a
taeleenz38 <taekyu.lee3@gmail.com>
2024-08-09 11:16:05 +1000

    funddescription CSS

--- frontend/src/app/components/organisms/FundDescription.tsx
+++ frontend/src/app/components/organisms/FundDescription.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const FundDescription = () => {
   return (
-    <div className="flex flex-col md:flex-row w-full mx-auto max-w-screen-xl py-8 md:py-14 px-8 text-gray">
+    <div className="flex flex-col md:flex-row w-full mx-auto max-w-screen-xl py-8 md:py-14 px-8 text-black">
       <div className="w-full md:w-1/3">
         <p className="text-base lg:text-lg font-semibold text-left">
           AYF is a tokenised Fund and is designed for investors seeking a
