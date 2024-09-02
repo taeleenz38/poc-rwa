7dc6eb096add3f23d23b449554a9005733f845dd
Randika Fernando <randika.fernando@igreendata.com.au>
2024-09-02 14:16:27 +1000

    Added .env to resolve issue in GCP envrionment.

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,9 +1,19 @@
 /** @type {import('next').NextConfig} */
-import { config } from 'dotenv';
+import dotenv from 'dotenv';
+import fs from 'fs';
 import path from 'path';
 
 console.log("CWD: " + process.cwd());
-config({ path: path.resolve(process.cwd(), './config/.env') });
+const __dirname = new URL('.', import.meta.url).pathname;
+const envPath = path.resolve(__dirname, 'config/.env');
+
+console.log("Env File Path: " + envPath);
+// config({ path: envPath });
+
+if (fs.existsSync(envPath)) {
+  dotenv.config({ path: envPath });
+}
+console.log('Environment Variable:', process.env.NEXT_PUBLIC_BACKEND_API);
 
 const nextConfig = {
   env: {
